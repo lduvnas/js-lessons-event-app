05f49cab5ca762c1d096fc2bcf895fa81ed41091
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2020-09-10 15:30:00 +0200

    Add links to eventPage

--- src/pages/EventListPage.jsx
+++ src/pages/EventListPage.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { UserContext } from "../contexts/UserContext";
 
 const ROOT_URL = "http://yoshi.willandskill.eu:8999/api/v1/";
@@ -35,7 +36,7 @@ export default function EventListPage() {
         eventList.map(eventItem => {
           return (
             <div key={eventItem.id}>
-              <p>{eventItem.title}</p>
+              <Link to={`event/${eventItem.slug}`}>{eventItem.title}</Link>
             </div>
           );
         })}
