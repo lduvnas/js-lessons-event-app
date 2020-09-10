cc81d75ac084b5de6b0dc5fbad4a2dabeece27f5
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2020-09-10 14:01:31 +0200

    Create EventListPage.jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/EventListPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function EventListPage() {
+  return (
+    <div>
+      <h1>Event List Page</h1>
+    </div>
+  );
+}
