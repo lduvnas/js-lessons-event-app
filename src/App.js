161359913c60080ef574b0a5b21030c3afcccc39
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2020-09-10 11:19:17 +0200

    Update App.js
    Remove boilerplate code.
    Add Login form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  function logIn(params) {
+    console.log("login");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Event App</h1>
+      <div>
+        <div>
+          <label>
+            Email <input name="email" placeholder="john.doe@company.com" />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Password{" "}
+            <input name="password" type="password" placeholder="password" />
+          </label>
+        </div>
+        <button onClick={logIn}>Login</button>
+      </div>
     </div>
   );
 }
