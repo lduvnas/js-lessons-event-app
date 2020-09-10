4af6713dc93a61d13e7eed203d80abd1b3649e46
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2020-09-10 11:33:40 +0200

    Start work with login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import React from "react";
+import React, { useRef } from "react";
+
+const ROOT_URL = "http://yoshi.willandskill.eu:8999/api/v1/";
+const LOGIN_URL = `${ROOT_URL}auth/api-token-auth/`;
 
 function App() {
-  function logIn(params) {
-    console.log("login");
+  const emailInput = useRef(null);
+  const passwordInput = useRef(null);
+
+  function logIn() {
+    fetch(LOGIN_URL);
   }
+
   return (
     <div>
       <h1>Event App</h1>
       <div>
         <div>
           <label>
-            Email <input name="email" placeholder="john.doe@company.com" />
+            Email
+            <input
+              ref={emailInput}
+              name="email"
+              placeholder="john.doe@company.com"
+            />
           </label>
         </div>
 
         <div>
           <label>
-            Password{" "}
-            <input name="password" type="password" placeholder="password" />
+            Password
+            <input
+              ref={passwordInput}
+              name="password"
+              type="password"
+              placeholder="password"
+            />
           </label>
         </div>
         <button onClick={logIn}>Login</button>
